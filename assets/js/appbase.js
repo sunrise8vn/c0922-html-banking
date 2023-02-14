415fdb35a1469e6c813c2b22814dea66e2eac80a
sunrise8vn <sunrise8vn@gmail.com>
2023-02-14 16:57:25 +0700

    Init

---
+++ assets/js/appbase.js
@@ -0,0 +1,63 @@
+class AppBase {
+    static DOMAIN_API = 'http://localhost:3300';
+
+    static SweetAlert = class {
+        static showDeleteConfirmDialog() {
+            return Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            })
+        }
+
+        static showSuccessAlert(t) {
+            Swal.fire({
+                icon: 'success',
+                title: t,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 2000
+            })
+        }
+
+        static showErrorAlert(t) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Warning',
+                text: t,
+            })
+        }
+
+        static showError401() {
+            Swal.fire({
+                icon: 'error',
+                title: 'Access Denied',
+                text: 'Invalid credentials!',
+            })
+        }
+
+        static showError403() {
+            Swal.fire({
+                icon: 'error',
+                title: 'Access Denied',
+                text: 'You are not authorized to perform this function!',
+            })
+        }
+    }
+}
+
+class Customer {
+    constructor(id, fullName, email, phone, address, balance, deleted) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.balance = balance;
+        this.deleted = deleted;
+    }
+}
